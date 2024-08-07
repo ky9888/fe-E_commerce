6c460b429e841425ef1e68fb4a22f8be3d70a4df
ky <nvk250698@gmail.com>
2024-08-07 21:34:25 +0700

    first commit

--- src/app/Airpods/[id]/page.tsx
+++ src/app/Airpods/[id]/page.tsx
@@ -1,32 +1,40 @@
-import ProductDetail from "../../../components/product/ProductDetail"
+import ProductDetail from "../../../components/product/ProductDetail";
 
-export type productType={
-  _id:string,
-  name:string,
-  selectImg:seletImgtype,
+export type ProductType = {
+  _id: string;
+  name: string;
+  selectImg: SelectImgType;
+};
 
-}
-export type seletImgtype={
-  title:string,
-  image:string,
- 
+export type SelectImgType = {
+  title: string;
+  image: string;
+};
 
-}
-
-
-export default async  function home({
-    params:{id}
-}:{
-    params:{id:string}
+export default async function Home({
+  params: { id }
+}: {
+  params: { id: string };
 }) {
-    const res =await fetch (`${process.env.URL}/products/getDetailProducts/${id}`,{cache: "no-store"})
-    const ress=await res.json();
-    const data=ress.data
-    console.log("check data",data);
-  return (
-    <>
-     <ProductDetail data={data} />
-      
-    </>
-  );
+  try {
+    const res = await fetch(`${process.env.URL}/products/getDetailProducts/${id}`, { cache: "no-store" });
+
+    if (!res.ok) {
+      throw new Error(`Lỗi khi lấy dữ liệu sản phẩm: ${res.statusText}`);
+    }
+
+    const ress = await res.json();
+    const data: ProductType = ress.data;
+    console.log("check data", data);
+
+    return (
+      <>
+        <ProductDetail data={data} />
+      </>
+    );
+  } catch (error) {
+    console.error('Đã xảy ra lỗi khi lấy chi tiết sản phẩm:', error);
+    // Bạn có thể chọn cách xử lý lỗi khác như hiển thị một thông báo lỗi cho người dùng
+    return <div>Đã xảy ra lỗi khi tải dữ liệu sản phẩm.</div>;
+  }
 }

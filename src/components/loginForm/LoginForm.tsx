ab6598042d2813d4748275f9d0a6ccf8481d7b27
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-08-09 11:42:51 +0700

    Update LoginForm.tsx

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -84,7 +84,7 @@ const LoginForm: React.FC = () => {
   };
 
   return (
-    <div className=" w-full flex justify-center  mt-[10%] px-[30%] max-lg:px-[7%] ">
+    <div className=" w-full flex justify-center  my-[5%] px-[30%] max-lg:px-[7%] ">
       <form
         className="rounded-lg space-y-2 w-full h-full shadow-[0_0px_70px_-5px_rgba(0,0,0,0.1)] p-5"
         onSubmit={formik.handleSubmit}

9ca11d01c97d735952997754fd3174cc5059cb03
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-09-17 11:51:15 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,22 @@
 import HederMain from "@/components/herderMain/HerderMain";
 import HomePage from "@/components/home/Home";
-import { HomePageProps } from "@/components/home/Home";
 
-export async function getServerSideProps() {
+export default async function home() {
   let data = [];
+
   try {
     const res = await fetch(`${process.env.URL}/products/getAllProducts`, { cache: "no-store" });
     const ress = await res.json();
     data = ress.data;
+    console.log("check data", data);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 
-  return {
-    props: {
-      data,
-    },
-  };
-}
-
-export default function Home({ data }:HomePageProps) {
   return (
     <div className="bg-slate-200 overflow-x-auto">
-      <div><HederMain /></div>
-      <div><HomePage data={data} /></div>
+      <div className=" "><HederMain/></div>
+      <div className=" "><HomePage data={data} /></div>
     </div>
   );
 }

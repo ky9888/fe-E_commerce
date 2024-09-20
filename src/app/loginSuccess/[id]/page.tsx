d20246bcb6fcef7f63d261138f691dcb0e244e3b
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-09-20 14:22:37 +0700

    Update page.tsx

--- src/app/loginSuccess/[id]/page.tsx
+++ src/app/loginSuccess/[id]/page.tsx
@@ -33,10 +33,10 @@ export default function Home({
         fetchData();
     }, []); // Adding `id` to dependencies
    return (
-    <div className=" flex justify-center">
-      <h1 className="text-center text-[35px] mt-[100px] text-red-500">
+    <div className=" ">
+      <p className="text-center  text-[35px] mt-[100px] text-red-500">
         loading...!
-      </h1>
+      </p>
     </div>
   );
   
